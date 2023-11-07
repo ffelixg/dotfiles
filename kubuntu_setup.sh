5bfea79a5abc2ab00e61b6addb2bc9d51e8728ec
ffelixg <142172984+ffelixg@users.noreply.github.com>
2023-11-07 12:08:41 +0100

    Update kubuntu_setup.sh

--- kubuntu_setup.sh
+++ kubuntu_setup.sh
@@ -12,11 +12,13 @@ sudo apt install -y zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
 # vs code
-mkdir /tmp/codeinst
-cd /tmp/codeinst
-wget https://code.visualstudio.com/sha/download?build=stable&os=linux-deb-x64
-mv * a.deb
-sudo apt install -y ./*
+sudo apt-get install wget gpg apt-transport-https
+wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > packages.microsoft.gpg
+sudo install -D -o root -g root -m 644 packages.microsoft.gpg /etc/apt/keyrings/packages.microsoft.gpg
+sudo sh -c 'echo "deb [arch=amd64,arm64,armhf signed-by=/etc/apt/keyrings/packages.microsoft.gpg] https://packages.microsoft.com/repos/code stable main" > /etc/apt/sources.list.d/vscode.list'
+rm -f packages.microsoft.gpg
+sudo apt update
+sudo apt install code # or code-insiders
 
 # conda
 mkdir -p ~/miniconda3
@@ -27,6 +29,8 @@ rm ~/miniconda3/miniconda.sh
 ~/miniconda3/bin/conda init zsh
 
 # startup.sh
+mkdir -p ~/.config/autostart/
 echo "qdbus org.kde.KWin /Compositor suspend" > ~/startup.sh # overrated feature
+qdbus org.kde.KWin /Compositor suspend
 chmod +x ~/startup.sh
-echo "[Desktop Entry]\nExec=~/startup.sh\nIcon=dialog-scripts\nName=startup.sh\nPath=\nType=Application\nX-KDE-AutostartScript=true" > ~/.config/autostart/startup.sh.desktop
+printf "[Desktop Entry]\nExec=~/startup.sh\nIcon=dialog-scripts\nName=startup.sh\nPath=\nType=Application\nX-KDE-AutostartScript=true" > ~/.config/autostart/startup.sh.desktop
