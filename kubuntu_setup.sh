1ea88986fea014ea38b1f21010f79e142152545e
ffelixg <142172984+ffelixg@users.noreply.github.com>
2024-05-08 19:03:25 +0200

    Update kubuntu_setup.sh

--- kubuntu_setup.sh
+++ kubuntu_setup.sh
@@ -23,6 +23,7 @@ sudo apt install code # or code-insiders
 # conda
 mkdir -p ~/miniconda3
 wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh -O ~/miniconda3/miniconda.sh
+# wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-aarch64.sh -O ~/miniconda3/miniconda.sh # for arm
 bash ~/miniconda3/miniconda.sh -b -u -p ~/miniconda3
 rm ~/miniconda3/miniconda.sh
 ~/miniconda3/bin/conda init bash
